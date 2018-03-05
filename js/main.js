a6ff4cbfa4acf32fd9ee396d5487fd1cc8255b87
alexzanotti <alexanderzanotti@hotmail.com>
2018-03-05 17:00:14 +0000

    added search jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 	$(".wrapper a").lightBox();
 });
+
+
+
+//this is where im currently stuck
+$("#search").keypress(function(){
+  var current_query = $("#search").val();
+
+  $(".image a").hide();
+
+  $(".image a").each(function(){
+
+    var current_keyword = $(this).attr("data-title");
+
+    if (current_keyword.indexOf("current_query") >= 0){
+
+      $(this).show();
+    }
+  });
+});
