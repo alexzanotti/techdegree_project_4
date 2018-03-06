a12513f8c511c83a13b157ecbca1d59f07f2a3de
alexzanotti <alexanderzanotti@hotmail.com>
2018-03-06 21:06:05 +0000

    updated index.html and correct main.js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,7 @@ $(document).ready(function() {
 	$(".wrapper a").lightBox();
 });
 
-$('#search').hideseek({
-    attribute: 'data-title'
-});
 
-
-
-/* this is where im currently stuck
 $("#search").keypress(function(){
   var current_query = $("#search").val();
 
@@ -18,29 +12,9 @@ $("#search").keypress(function(){
 
     var current_keyword = $(this).attr("data-title");
 
-    if (current_keyword.indexOf("current_query") >= 0){
+    if (current_keyword.indexOf(current_query) >= 0){
 
       $(this).show();
     }
   });
 });
-
-*/
-
-/* this is where im currently stuck
-$("#search").keypress(function(){
-  var current_query = $("#search").val();
-
-  $(".image a").hide();
-
-  $(".image a").each(function(){
-
-    var current_keyword = $(this).attr("data-title");
-
-    if (current_keyword.indexOf("current_query") >= 0){
-
-      $(this).show();
-    }
-  });
-});
-*/
